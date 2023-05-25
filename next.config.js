1996c8c9cf0bb6e6eebce48adc288796caf60965
Golam <rabby29hossain@gmail.com>
2023-05-24 22:43:14 -0400

    next-auth.config file added with

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
    images: {
       domains: ["res.cloudinary.com"],
    },
-   cookies: {
-      domain: "https://amazona-7bnd8djpd-golamhossain9355.vercel.app",
-   },
 }
 
 module.exports = nextConfig
